58fd50dea722bf5111e3012a35e27126d94d6d79
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-05 22:06:48 +0700

    rs: add cut/move/move.any stubs

--- lib-rs/src/es_rs.rs
+++ lib-rs/src/es_rs.rs
@@ -7,17 +7,18 @@ pub use ::h::alias 	::*;
 pub use ::h::helper	::*;
 
 #[path="libmod/[libmod].rs"] pub mod libmod;
-use crate::libmod::{sum,used_memory,hello,console,setup_logging};
+use crate::libmod::{used_memory,setup_logging,cut,move_cb_to,ask_name};
 
 use mlua::prelude::*;
 
 // check ./.cargo/config.toml for macOS specific compiler flags
 #[mlua::lua_module(name="libes_rs")] //register Lua module entrypoint, MUST match require(module_file_name)
-fn my_lua_lib(lua:&Lua) -> LuaResult<LuaTable> { // exported as _luaopen_libes_rs, see `nm -gU libes_rs.rs`
+fn my_lua_lib(lua:&Lua) -> LuaResult<LuaTable> { // exported as _luaopen_libes_rs, see `nm -gU libes_rs.dylib`
   setup_logging()?;
   let exports = lua.create_table()?;
-  exports.set("sum"        , lua.create_function(sum        )?)?;
-  exports.set("used_memory", lua.create_function(used_memory)?)?;
-  exports.set("console"    , lua.create_function(console    )?)?;
+  exports.set("used_memory"	, lua.create_function(used_memory	)?)?;
+  exports.set("cut"        	, lua.create_function(cut        	)?)?;
+  exports.set("move_to"    	, lua.create_function(move_cb_to 	)?)?;
+  exports.set("ask_name"   	, lua.create_function(ask_name   	)?)?;
   Ok(exports)
 }
