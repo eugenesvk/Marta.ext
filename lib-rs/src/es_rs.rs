a70f63e91748e22879dbcde4454b98a7d8f6af90
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-05 19:20:33 +0700

    add lua module with test functions

--- lib-rs/src/es_rs.rs
+++ lib-rs/src/es_rs.rs
@@ -7,8 +7,17 @@ pub use ::h::alias 	::*;
 pub use ::h::helper	::*;
 
 #[path="libmod/[libmod].rs"] pub mod libmod;
-use crate::libmod::ret42;
+use crate::libmod::{sum,used_memory,hello,console,setup_logging};
 
-pub fn lib() -> i32 {
-  ret42()
+use mlua::prelude::*;
+
+// check ./.cargo/config.toml for macOS specific compiler flags
+#[mlua::lua_module(name="libes_rs")] //register Lua module entrypoint, MUST match require(module_file_name)
+fn my_lua_lib(lua:&Lua) -> LuaResult<LuaTable> { // exported as _luaopen_libes_rs, see `nm -gU libes_rs.rs`
+  setup_logging()?;
+  let exports = lua.create_table()?;
+  exports.set("sum"        , lua.create_function(sum        )?)?;
+  exports.set("used_memory", lua.create_function(used_memory)?)?;
+  exports.set("console"    , lua.create_function(console    )?)?;
+  Ok(exports)
 }
--- lib-rs/src/libmod/[libmod].rs
+++ lib-rs/src/libmod/[libmod].rs
@@ -1,1 +1,19 @@
-pub fn ret42() -> i32 { 42 }
+use mlua::prelude::*;
+pub fn sum        (_  :&Lua, (a,b):(i64,i64)) -> LuaResult<i64  > {Ok(a + b)            }
+pub fn used_memory(lua:&Lua, _    :(       )) -> LuaResult<usize> {Ok(lua.used_memory())}
+
+use tracing::{info,warn,Level};
+use tracing_subscriber::prelude::*; // added error check
+use tracing_oslog::OsLogger;
+const log_subsystem:&'static str = "Marta.es_rs";
+const log_category :&'static str = "default";
+pub fn setup_logging() -> LuaResult<()> {
+  let collector = tracing_subscriber::registry().with(OsLogger::new(log_subsystem,log_category));
+  tracing::subscriber::set_global_default(collector).expect("failed to set global subscriber"); //⚠️ libs should avoid this to not cause conflicts when executables that depend on the library try to set the default later
+  Ok(())
+}
+
+pub fn console(lua:&Lua,in_str:String) -> LuaResult<i8> {
+  warn!(myvar="⚠️","fn console(in_str)@Marta's Rust lua module es_rs.rs, in_str=‘{:?}’", in_str); // this creates a new event, outside of any spans.
+  Ok(0)
+}
