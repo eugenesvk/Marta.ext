9681b421e3c032e4bacc4a8b7b2b4c9fdc671556
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-07 20:12:51 +0700

    __delete add dummy bin clipboar

--- lib-rs/src/dummy.bin.rs
+++ lib-rs/src/dummy.bin.rs
@@ -12,8 +12,18 @@ use crate::binmod::print42;
 use std::error::Error;
 use std::result;
 
+use std     	::{//env,fs,
+  path      	::{Path,PathBuf},
+  // process	::{Command,Stdio},
+};
 type Result<T> = result::Result<T, Box<dyn Error>>;
 fn main() -> Result<()> {
   print42()?;
+
+  use clipboard_files;
+  match clipboard_files::read() { //:Vec<std::path::PathBuf>
+    Ok (paths)	=> {p!("got №{} paths: {:?}",paths.len(),paths)?; p!("{}", type_of(paths))?;},
+    Err(e)    	=> {p!("not files")?;},
+  }
   Ok(())
 }
