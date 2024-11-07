bd4bbf2af79d5e8c8f612ab7f50d10eedd12f35b
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-08 01:06:15 +0700

    update es cut

--- Plugins/es¦cut/init.lua
+++ Plugins/es¦cut/init.lua
@@ -18,20 +18,36 @@ marta.action({id="📋paste.move.any",name="Paste&move any items from the 📋cl
   isApplicable = function(ctxA) return ctxA.activePane.model.hasActiveFiles end,
   apply        = function(ctxA) clipboard_cut_paste({ctxA=ctxA,op="move.any"})  ; end})
 
+local _d = 1
+
 is_cut = false
 function clipboard_cut_paste(arg)
-  local ctxA    	= arg.ctxA             	-- holds refs to PaneContext instances for active+inactive panes
-  local ctxG    	= marta.globalContext  	--
+  local ctxA    	= arg.ctxA	-- holds refs to PaneContext instances for active+inactive panes
+  local ctxW    	= ctxA.window
+  local ctxG    	= marta.globalContext	--
+  local actG    	= ctxG.actions
   local fsL     	= marta.localFileSystem	--
   local ctxPA   	= ctxA.activePane      	--
   local ctxP_inA	= ctxA.inactivePane    	--
   local model   	= ctxPA.model          	-- Active pane list model
   local viewP   	= ctxPA.view           	--
-  local filesInf	= model.activeFileInfos	-- array of FileInfo with all the attributes, gathered on folder load (so cached) (ZIP fs doesn't store macOS extended attributes)
-  local parentFd	= model.folder         	--
-  if     (arg.op == "cut"     ) then is_cut = true ; libes_rs.cut     () -- set our global flag to true to track our cut
-  elseif (arg.op == "move"    ) then is_cut = false; libes_rs.move    ()
-  elseif (arg.op == "move.any") then is_cut = false; libes_rs.move_any()
-  end
 
+  function pss(msg) viewP:showNotification(msg,plugID,"short") end -- short-term "print" to the statusbar
+  function psl(msg) viewP:showNotification(msg,plugID,"long" ) end -- long-term  "print" to the statusbar
+  function dbg(l,msg) if (_d>=l) then pss(msg) end end -- short-term "print" to the statusbar
+
+  function run_action(action) ctxW:runAction(actG:getById(action),ctxPA) end -- short-term "print" to the statusbar
+
+  function pss(msg) viewP:showNotification(msg,plugID,"short") end -- short-term "print" to the statusbar
+  -- set our global flag to true to track our cut ↓, but otherwise ↓ use native copy libes_rs.cut()
+  if     (arg.op == "cut"     ) then is_cut = true ; run_action("core.clipboard.copy"); pss('📋cut')
+  elseif (arg.op == "move"    ) then
+    if   (is_cut == true      ) then is_cut = false; libes_rs.move()                  ; dbg(1,'‘📋paste.move.cut’')
+    else pss("No items were ‘📋cut’, so nothing to ‘📋paste.move.cut’")
+    end
+  elseif (arg.op == "move.any") then
+    if   (is_cut == true      ) then is_cut = false; --libes_rs.move_any()
+    else run_action("core.clipboard.paste"); pss("No items were ‘📋cut’, pasting copied…")
+    end
+  end
 end
