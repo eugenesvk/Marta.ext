7e04448560052693b2df4463a47ce61583556f4a
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-07 20:20:17 +0700

    __del reformat

--- lib-rs/src/libmod/[libmod].rs
+++ lib-rs/src/libmod/[libmod].rs
@@ -36,34 +36,60 @@ pub fn call_lua_fn(lua:&Lua, _:()) -> LuaResult<LuaString> { // example of using
   let s_rs:String = mytostring.call::<String>(123)?;
   let s_lua:LuaString = lua.create_string(s_rs)?;
 
+  //1
   let s_lua1:LuaString = lua.create_string("glue.get(myalert).call::<()>(s_lua1)")?;
-  //1ok
+  //1a ok
   // let myalert:Function = g.get("myalert")?;
   // let _ = myalert.call::<()>(s_lua1)?;
-  //2
+  //1b
   // let got:LuaString = match g.get::<LuaValue>("myalert")? {
   //   LuaValue::Function(myalert)	=> myalert.call((s_lua1,)),
   //   _                          	=> Err(LuaError::RuntimeError("❗ globals.myalert wrong type ≠ 𝑓𝑛".into())),
   //   }?;
   //missing myalert1 shows no context in Console from Marta -- 'apply()' failed:LuaUserData(ptr:Optional(0x0000600001071cb8))
-  //3 custom console error message works, but how to make LuaError show up? Is it blocked by Marta???
-  let got = match g.get::<LuaValue>("myalert1") {
+  //1c custom console error message works, but how to make LuaError show up? Is it blocked by Marta???
+  match g.get::<LuaValue>("myalert1") {
     Ok(val)                      	=> match val {
+      LuaValue::Nil              	=> {warn!("❗ globals.myalert1 not found"); Ok(())},
       LuaValue::Function(myalert)	=> myalert.call::<()>(s_lua1),
-      _                          	=> {warn!("❗ globals.myalert1 not found"); Err(anyhow!("❗"))},
+      _                          	=> {warn!("❗ globals.myalert1 wrong type 𝑓𝑛 ≠ {:?}",type_of(val)); Ok(())},
+      // _                       	=> {warn!("❗ globals.myalert1 wrong type ≠ 𝑓𝑛 {:?}",type_of(val)); Err(anyhow!("❗"))}?,
       // _                       	=> {warn!("❗ globals.myalert1 not found"); Err(LuaError::RuntimeError("❗".into()))},
       }                          	,
-    _                            	=> {warn!("❗ globals.myalert1 wrong type ≠ 𝑓𝑛"); Err(LuaError::RuntimeError("❗".into()))},
+    // _                         	=> {warn!("❗ globals.myalert1 not found"); Err(anyhow!("❗".into()))}?,
+    _                            	=> {warn!("❗ globals.myalert1 failed"); Ok(())},
     }?;
 
-  let s_lua2:LuaString = lua.create_string("g.get(martax).get(alert).call::<()>(s_lua2)")?;
+  //2
+  // let s_lua2:LuaString = lua.create_string("g.get(martax).get(alert).call::<()>(s_lua2)")?;
+  // let martax:LuaTable = g.get("martax")?;
+  // let alert:Function = martax.get("alert")?;
+  // let _ = alert.call::<()>(s_lua2)?;
+
+  //3
+  // let get_application_url:Function = martax.get("getApplicationUrl")?;
+  // let s_app:LuaString = get_application_url.call::<LuaString>("com.apple.calculator")?;
+  // let _ = alert.call::<()>(s_app)?;
+
+  //4 get files from clipboard and alert via martax fn
+  let mut out_s:String = "get files from clipboard and alert via martax fn\n".to_string();
+  out_s.push_str("g.get(martax).get(alert).call::<()>(s_lua2)\n");
+  use std     	::{//env,fs,
+    path      	::{Path,PathBuf},
+    // process	::{Command,Stdio},
+  };
+  use clipboard_files;
+  match clipboard_files::read() { //:Vec<std::path::PathBuf>
+    Ok (paths)	=> {out_s.push_str(format!("got №{}",paths.len()).as_ref());},
+    Err(e)    	=> {out_s.push_str("clipboard has no files");},
+  }
   let martax:LuaTable = g.get("martax")?;
   let alert:Function = martax.get("alert")?;
+  let s_lua2:LuaString = lua.create_string(out_s)?;
   let _ = alert.call::<()>(s_lua2)?;
 
-  let get_application_url:Function = martax.get("getApplicationUrl")?;
-  let s_app:LuaString = get_application_url.call::<LuaString>("com.apple.calculator")?;
-  let _ = alert.call::<()>(s_app)?;
+  Ok(s_lua)
+}
 
 use std     	::{//env,fs,
   path      	::{Path,PathBuf},
