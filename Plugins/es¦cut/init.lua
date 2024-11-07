201c370481bb9733411618248999a4ebcd73fd89
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-08 01:06:15 +0700

    update es cut

--- Plugins/es¦cut/init.lua
+++ Plugins/es¦cut/init.lua
@@ -11,27 +11,42 @@ local libes_rs	= require "libes_rs" -- name = `#[mlua::lua_module(name="es_rs")]
 marta.action({id="📋cut",name="Cut the currently selected items to the 📋clipboard ()"  ,
   isApplicable = function(ctxA) return ctxA.activePane.model.hasActiveFiles end,
   apply        = function(ctxA) clipboard_cut_paste({ctxA=ctxA,op="cut"})  ; end})
-marta.action({id="📋paste.move.cut",name="Paste&move the previously ‘📋cut’ items to the currently active tab (ignore items added directly via ‘core.clipboard.copy’)"  ,
+marta.action({id="📋paste.move.cut",name="Move the previously ‘📋cut’ items to the currently active tab or paste copied items"  ,
   isApplicable = function(ctxA) return ctxA.activePane.model.hasActiveFiles end,
   apply        = function(ctxA) clipboard_cut_paste({ctxA=ctxA,op="move"})  ; end})
 marta.action({id="📋paste.move.any",name="Paste&move any items from the 📋clipboard to the currently active tab (even if not added via ‘📋cut’, but ‘core.clipboard.copy’)"  ,
   isApplicable = function(ctxA) return ctxA.activePane.model.hasActiveFiles end,
   apply        = function(ctxA) clipboard_cut_paste({ctxA=ctxA,op="move.any"})  ; end})
 
+local _d = 1
+
 is_cut = false
 function clipboard_cut_paste(arg)
-  local ctxA    	= arg.ctxA             	-- holds refs to PaneContext instances for active+inactive panes
-  local ctxG    	= marta.globalContext  	--
+  local ctxA    	= arg.ctxA	-- holds refs to PaneContext instances for active+inactive panes
+  local ctxW    	= ctxA.window
+  local ctxG    	= marta.globalContext	--
+  local actG    	= ctxG.actions
   local fsL     	= marta.localFileSystem	--
   local ctxPA   	= ctxA.activePane      	--
   local ctxP_inA	= ctxA.inactivePane    	--
   local model   	= ctxPA.model          	-- Active pane list model
   local viewP   	= ctxPA.view           	--
-  local filesInf	= model.activeFileInfos	-- array of FileInfo with all the attributes, gathered on folder load (so cached) (ZIP fs doesn't store macOS extended attributes)
-  local parentFd	= model.folder         	--
-  if     (arg.op == "cut"     ) then is_cut = true ; libes_rs.cut     () -- set our global flag to true to track our cut
-  elseif (arg.op == "move"    ) then is_cut = false; libes_rs.move    ()
-  elseif (arg.op == "move.any") then is_cut = false; libes_rs.move_any()
-  end
 
+  function pss(msg) viewP:showNotification(msg,plugID,"short") end -- short-term "print" to the statusbar
+  function psl(msg) viewP:showNotification(msg,plugID,"long" ) end -- long-term  "print" to the statusbar
+  function dbg(l,msg) if (_d>=l) then pss(msg) end end -- short-term "print" to the statusbar
+
+  function run_action(action) ctxW:runAction(actG:getById(action),ctxPA) end -- short-term "print" to the statusbar
+
+  function pss(msg) viewP:showNotification(msg,plugID,"short") end -- short-term "print" to the statusbar
+  -- set our global flag to true to track our cut ↓, but otherwise ↓ use native copy libes_rs.cut()
+  if     (arg.op == "cut"     ) then is_cut = true ; run_action("core.clipboard.copy"); pss('📋cut')
+  elseif (arg.op == "move"    ) then
+    if   (is_cut == true      ) then is_cut = false; libes_rs.move()                  ; dbg(1,'‘📋paste.move.cut’')
+    else run_action("core.clipboard.paste");             dbg(1,"No items were ‘📋cut’, pasting copied…")
+    end
+  elseif (arg.op == "move.any") then
+    if   (is_cut == true      ) then is_cut = false else dbg(1,"No items were ‘📋cut’, moving copied…") end
+    libes_rs.move()
+  end
 end
