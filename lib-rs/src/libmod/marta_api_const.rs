d1367c813cacd6e70b9203eb8313e28917f18a8b
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-12 01:01:13 +0700

    __add const

--- lib-rs/src/libmod/marta_api_const.rs
+++ lib-rs/src/libmod/marta_api_const.rs
@@ -1,19 +1,19 @@
 // Associated constant with a struct for autocomplete and typo avoidance
-trait C_ActionContext {
+pub trait C_ActionContext {
   const activePane  	:&str ="activePane"  	;
   const args        	:&str ="args"        	;
   const inactivePane	:&str ="inactivePane"	;
   const window      	:&str ="window"      	;
   const toWeak      	:&str ="toWeak"      	;
 }
-trait C_WindowContext { //marta.sh/api/marta/windowcontext.type/
+pub trait C_WindowContext { //marta.sh/api/marta/windowcontext.type/
   const isEnabled	:&str = "isEnabled"	;//let : Boolean
   const nsWindow 	:&str = "nsWindow" 	;//let : LightUserData<NSWindow>
   const panes    	:&str = "panes"    	;//let : PaneManager
   const tabs     	:&str = "tabs"     	;//let : TabManager
   const runAction	:&str = "runAction"	;//fun (action, context)
 }
-trait C_PaneContext {
+pub trait C_PaneContext {
   const id           	:&str ="id"           	;//let: String
   const model        	:&str ="model"        	;//let: ListModel
   const tabIndex     	:&str ="tabIndex"     	;//let: Int
@@ -21,7 +21,7 @@ trait C_PaneContext {
   const windowContext	:&str ="windowContext"	;//let: WindowContext
   const activateTab  	:&str ="activateTab"  	;//fun()
 }
-trait C_PaneView { //marta.sh/api/marta/paneview.type/
+pub trait C_PaneView { //marta.sh/api/marta/paneview.type/
   const currentItemRect         	:&str	="currentItemRect"         	;//let : Rect
   const addStatusText           	:&str	="addStatusText"           	;//fun (text, id, position)
   const ensureCurrentItemVisible	:&str	="ensureCurrentItemVisible"	;//fun ()
