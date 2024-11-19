b95857bc2bf390ee624d59ed56191f99dbd7224d
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-20 01:56:04 +0700

    rs: fix deprecation errors

--- lib-rs/src/libmod/gui/[gui].rs
+++ lib-rs/src/libmod/gui/[gui].rs
@@ -11,7 +11,8 @@ pub use overwrite	::*;
 use cacao::appkit::{App, AppDelegate};
 use cacao::appkit::window::{Window,WindowConfig,WindowDelegate, Window as Win, WindowConfig as WinCfg, WindowDelegate as WinDelegate};
 
-use cacao::foundation::{id, nil, to_bool, NSInteger, NSString, NSUInteger, NO, YES, AutoReleasePool};
+use cacao::foundation::{id, nil, to_bool, NSInteger, NSString, NSUInteger, AutoReleasePool};
+use objc::ffi::{NO,YES};
 use objc::runtime::Object;
 use objc::{class, msg_send, sel};
 use objc_id::{ShareId,Id,Shared};
