011d8283c52bae54d6bb3fce1fb1f6b4199c1c6b
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-20 01:48:27 +0700

    rs: upd overwrite

--- lib-rs/src/libmod/gui/overwrite.rs
+++ lib-rs/src/libmod/gui/overwrite.rs
@@ -1,4 +1,3 @@
-//! The main Todos window
 use std::{
   ffi::c_void,
   sync::RwLock};
@@ -35,37 +34,117 @@ impl       wOverwrite {pub fn new() -> Self {wOverwrite {content:ViewController:
 
 // 2 Root view controller
 use cacao::{
-  layout	::{Layout,LayoutConstraint},
-  text  	::{Label,TextAlign},
-  view  	::{View,ViewDelegate,ViewController},
+  layout 	::{Layout,LayoutConstraint},
+  text   	::{Label,TextAlign},
+  view   	::{View,ViewDelegate,ViewController},
+  switch 	::Switch,
+  button 	::{Button,BezelStyle, BezelStyle as Border},
+  control	::{Control,ControlSize,},
+  color  	::Color,
 };
 
-#[derive(Debug)] pub struct vOverwrite {label:Label}
-impl Default      for       vOverwrite {fn default() -> Self {vOverwrite {label:Label::default()}}}
-impl ViewDelegate for       vOverwrite {const NAME: &'static str = "vOverwrite";
-  fn did_load(&mut self, v:View) { // when the View is ready to work with. You're passed a View - this is safe to store and use repeatedly, but it's not thread safe - any UI calls must be made from the main thread!
-    // warn!("ViewDelegate for vOverwrite did_load function");
-    self.label.set_text("Label vOverwrite");
-    self.label.set_text_alignment(TextAlign::Center);
-    v.add_subview(&self.label);
+#[derive(Debug)] pub struct vOverwrite {
+  pub v       	: View  	,//
+  pub title   	: Label 	,//
+  pub subtitle	: Label 	,//
+  pub bYes    	: Button	,//
+  pub bNo     	: Button	,//
+}
+impl Default      for       vOverwrite { /// Creates and returns a stock Overwrite view
+  fn default() -> Self {
+    let v       	= View::new();
+    let title   	= Label::new()	;//v.add_subview(&title   	);
+    let subtitle	= Label::new()	;//v.add_subview(&subtitle	);
+
+    let mut y=Button::new("❗Overwrite"	);y.set_action(|_| dispatch_ui(Message::MoveOverwrite	));y.set_key_equivalent("\r");//🖍
+    let mut n=Button::new("Cancel"    	);n.set_action(|_| dispatch_ui(Message::MoveCancel   	));n.set_key_equivalent("a");
+    y.set_highlighted(false);y.set_bezel_style(BezelStyle::RegularSquare);y.set_control_size(ControlSize::Large);y.set_text_color(Color::SystemRed  );
+    n.set_highlighted(true );n.set_bezel_style(BezelStyle::RegularSquare);n.set_control_size(ControlSize::Large);n.set_text_color(Color::SystemBlack);
+    v.add_subview(&y);let bYes = y;
+    v.add_subview(&n);let bNo  = n;
 
     LayoutConstraint::activate(&[
-      self.label.top     	.constraint_equal_to(&v.top     	).offset( 100.),
-      self.label.leading 	.constraint_equal_to(&v.leading 	).offset(  16.),
-      self.label.trailing	.constraint_equal_to(&v.trailing	).offset( -16.),
-      self.label.bottom  	.constraint_equal_to(&v.bottom  	).offset(-100.),
+      // title   	.top     	.constraint_equal_to(&v.top          	).offset(  2.),
+      // subtitle	.top     	.constraint_equal_to(&title.bottom   	),
+      // // bYes 	.top     	.constraint_equal_to(&subtitle.bottom	).offset(-20.),
+      // // bNo  	.top     	.constraint_equal_to(&subtitle.bottom	).offset(-20.),
+      // title   	.leading 	.constraint_equal_to(&v.leading      	),
+      // subtitle	.leading 	.constraint_equal_to(&v.leading      	),
+      // // bYes 	.leading 	.constraint_equal_to(&v.leading      	).offset( -4.),
+      // // bNo  	.leading 	.constraint_equal_to(&v.leading      	).offset( -4.),
+      // title   	.trailing	.constraint_equal_to(&v.trailing     	),
+      // subtitle	.trailing	.constraint_equal_to(&v.trailing     	),
+      // title   	.width   	.constraint_equal_to_constant(248.   	),
+      // subtitle	.width   	.constraint_equal_to_constant(248.   	),
+      // // bYes 	.width   	.constraint_equal_to_constant(248.   	),bYes	.height	.constraint_equal_to_constant(148.	),
+      // // bNo  	.width   	.constraint_equal_to_constant(248.   	),
+      // subtitle	.bottom  	.constraint_equal_to(&v.bottom       	),
+      // // bYes 	.bottom  	.constraint_equal_to(&v.bottom       	),
+      // bNo  	.bottom  	.constraint_equal_to(&v.bottom       	),
+
+      v.width.constraint_equal_to_constant(600.0),
+      v.height.constraint_equal_to_constant(500.0),
+      bYes	.width   	.constraint_equal_to_constant(28.	),
+      bYes	.height  	.constraint_equal_to_constant(28.	),
+      bYes	.top     	.constraint_equal_to(&v.top).offset(-1.),
+      bYes	.leading 	.constraint_equal_to(&v.leading).offset(40.),
+      bYes	.trailing	.constraint_equal_to(&v.trailing).offset(-2.),
+      bYes	.bottom  	.constraint_equal_to(&v.bottom).offset(-1.),
     ]);
+    vOverwrite {v,title,subtitle,bYes,bNo}
+  }
+}
+// impl                        vOverwrite {
+//   /// Configures vOverwrite, handler is fired on each state change of the checkbox
+//   pub fn configure<F>(&mut self, text:&str, subtitle:&str, state:bool, handler:F) where F:Fn()+Send+Sync+'static {
+//     self.title   	.set_text(text);
+//     self.subtitle	.set_text(subtitle);
+//     self.switch  	.set_action(handler);
+//     self.switch  	.set_checked(state);
+//   }
+// }
+
+pub fn toggle_do_nothing() {}
+impl ViewDelegate for       vOverwrite {const NAME: &'static str = "vOverwrite";
+  fn did_load(&mut self, v:View) { //View is ready to work with, arg View is safe to store and use repeatedly, but it's not thread safe - any UI calls must be made from the main thread!
+    warn!("ViewDelegate for vOverwrite did_load function");
+    self.title      	.set_text          	("title_Label_vOverwrite"   	);
+    self.subtitle   	.set_text          	("subtitle_Label_vOverwrite"	);
+    // self.subtitle	.set_text_alignment	(TextAlign::Center          	);
+    v.add_subview(&self.v);
+
+    // LayoutConstraint::activate(&[
+      // v.width.constraint_equal_to_constant(1000.0),
+
+      // self.bYes	.top     	.constraint_equal_to(&v.top).offset(-2.),
+      // self.bYes	.leading 	.constraint_equal_to(&v.leading).offset(-2.),
+      // self.bYes	.trailing	.constraint_equal_to(&v.trailing).offset(-2.),
+      // self.bYes	.width   	.constraint_equal_to_constant(28.	),
+      // self.bYes	.height  	.constraint_equal_to_constant(28.	),
+      // self.bYes	.bottom  	.constraint_equal_to(&v.bottom).offset(-1.)
+
+      // self.bYes	.top     	.constraint_equal_to(&self.subtitle.bottom).offset(1.),
+      // self.bYes	.trailing	.constraint_equal_to(&v.trailing).offset(-40.),
+      // self.bYes	.width   	.constraint_equal_to_constant(248.	),
+      // self.bYes	.bottom  	.constraint_equal_to(&v.bottom).offset(-10.)
+    // ]);
+    // LayoutConstraint::activate(&[
+    //   self.label.top     	.constraint_equal_to(&v.top     	).offset( 100.),
+    //   self.label.leading 	.constraint_equal_to(&v.leading 	).offset(  16.),
+    //   self.label.trailing	.constraint_equal_to(&v.trailing	).offset( -16.),
+    //   self.label.bottom  	.constraint_equal_to(&v.bottom  	).offset(-100.),
+    // ]);
   }
 }
 
 impl WinDelegate for wOverwrite {const NAME: &'static str = "wOverwrite";
   fn did_load(&mut self, win:Win) { // when this window has loaded in memory, and is about to display (set up your views here)
     // warn!("WinDelegate for wOverwrite did_load function");
-    win.set_title                	("wOverwriteTitle"	);
-    win.set_autosave_name        	("wOverwrite"     	);
-    win.set_minimum_content_size 	(400, 400       	);
-    win.set_movable_by_background	(true           	);
-    win.set_title                	("Tasks"        	);
+    win.set_title                  	("wOverwriteTitle"	);
+    win.set_autosave_name          	("wOverwrite"     	);
+    // win.set_minimum_content_size	(100, 100         	);//min size this window can shrink to, no max
+    win.set_minimum_size           	(600, 500         	);//min/max match content
+    win.set_movable_by_background  	(true             	);
 
     win.set_content_view_controller(&self.content);
 
